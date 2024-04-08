e440fce42e33e514b12e39cba3d63a7ebc17a278
Rajesh Trasula <98252855+Rajeshtrasula@users.noreply.github.com>
2024-04-08 23:08:30 +0530

    Create Mental-Health.sql

---
+++ Mental-Health.sql
@@ -0,0 +1,14 @@
+-- Run this code to save the CSV file as students
+SELECT * 
+FROM 'students.csv';
+
+-- Find the number of international students and their average scores by length of stay, in descending order of length of stay
+SELECT stay, 
+       COUNT(*) AS count_int,
+       ROUND(AVG(todep), 2) AS average_phq, 
+       ROUND(AVG(tosc), 2) AS average_scs, 
+       ROUND(AVG(toas), 2) AS average_as
+FROM students
+WHERE inter_dom = 'Inter'
+GROUP BY stay
+ORDER BY stay DESC;
